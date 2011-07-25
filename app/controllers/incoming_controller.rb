69422fe61196e6c5f44547163ae1c0d4bdc08e07
Caley Woods <caley.w@gmail.com>
2011-07-25 00:05:34 -0500

    getting warmer

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,16 +2,16 @@ class IncomingController < ApplicationController
   BASE_URL = 'http://582dc2b5.dotcloud.com/'
 
   def text
-    @user = User.find_by_phone_number(params[:From])
+    @user = User.find_by_phone_number(params[:From][1..-1])
     @user.contacts.each {|contact| contact.notify}
 
     render :xml => {:sms => "Message received, contacts notified. Be safe!"}.to_xml(:root => 'Response')
   end
 
   def call
-    @user = User.find_by_phone_number(params[:From])
+    @user = User.find_by_phone_number(params[:From][1..-1])
     @user.contacts.each {|contact| contact.notify}
 
-    render :xml => {:say => "Hello, you got it"}.to_xml
+    render :xml => {:say => "Hello, you got it"}.to_xml(:root => 'Response')
   end
 end
